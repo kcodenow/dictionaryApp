fcdbe60f5406f789a2f74af71bd6939113d7da57
kevin clarke <kevinpatrickclarke@gmail.com>
2021-03-17 12:55:27 -0400

    abbreviations & names causing issues

--- dictionary.py
+++ dictionary.py
@@ -5,13 +5,17 @@ data = json.load(open('data.json'))
 
 def retrieve(word):
 	if word.lower() in data.keys():
-		return data[word]
+		return data[word.lower()]
+	elif word.upper() in data.keys(): # abbreviations like USA
+		return data[word.upper()]
+	elif word.capitalize() in data: # names, countries, like America
+		return data[word.capitalize()]
 	elif len(difflib.get_close_matches(word,data.keys())) > 0:
 		possibles = difflib.get_close_matches(word, data.keys(), n=3, cutoff=0.8)
-		for sug in possibles:
-			response = input('Did you mean: {} instead?'.format(sug))
+		for suggestion in possibles:
+			response = input('Did you mean: {} instead?\n'.format(suggestion))
 			if(response.lower() in ['y', 'yes']):
-				return data[sug]
+				return data[suggestion]
 	return None
 
 print('Welcome to the Thesaurus ...')
