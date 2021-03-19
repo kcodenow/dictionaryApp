6c343912b1cec4fae47bb0c9a1b5fbefb27ad3d0
kevin clarke <kevinpatrickclarke@gmail.com>
2021-03-19 15:34:51 -0400

    suggestions in GUI added, quit btn

--- gui.py
+++ gui.py
@@ -2,9 +2,14 @@ from tkinter import Tk, Button, Entry, Label, messagebox, StringVar, Text, END
 import json
 import difflib
 
-def buttonCmd():
+def searchBtnCmd():
 	result = lookup(entryVal.get())
-	text.insert(END, result)
+	text.delete(1.0, END)
+	if(result):
+		for line in result:
+			text.insert(END, '- ' + line + '\n')
+	else:
+		text.insert(END, 'Sorry - word cannot be found')
 
 def lookup(word):
 	if word.lower() in data.keys():
@@ -16,30 +21,25 @@ def lookup(word):
 	elif len(difflib.get_close_matches(word,data.keys())) > 0:
 		possibles = difflib.get_close_matches(word, data.keys(), n=3, cutoff=0.8)
 		for suggestion in possibles:
-			messagebox.askquestion('Hmmmmm ...', 'Did you mean {}?'.format(suggestion))
-			if(messagebox == 'yes'):
+			response = messagebox.askyesno('Hmmmmm ...', 'Did you mean {}?'.format(suggestion))
+			if(response):
 				return data[suggestion]
-				break
-	return 'Word could not be found'
+	return None
 
 data = json.load(open('data.json'))
 
 window = Tk()
 
-l1 = Label(window, text='Enter your word')
-l1.grid(row=0, column=0)
-
-l1 = Label(window, text='Result')
-l1.grid(row=0, column=2)
-
 entryVal = StringVar()
 entry = Entry(window, textvariable=entryVal)
-entry.grid(row=2, column=0, rowspan=2)
+entry.pack()
 
-b1 = Button(window, text='Search', command=buttonCmd)
-b1.grid(row=2, column=1, rowspan=2)
+b1 = Button(window, text='Search', command=searchBtnCmd)
+b1.pack()
 
-text = Text(window, height=1, width=20)
-text.grid(row=2, column=2)
+text = Text(window)
+text.pack()
 
+qButton = Button(window, text='Quit', command=window.destroy)
+qButton.pack()
 window.mainloop()
